b649688f79d45f4995943230e9d728ea26eb7611
Гамзатов Ибрагим <ibr.gamzatov.090905@mail.ru>
2025-03-21 13:51:26 +0300

    complete

--- src/main/java/web/dao/UserDAOImpl.java
+++ src/main/java/web/dao/UserDAOImpl.java
@@ -1,18 +1,30 @@
 package web.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import web.model.User;
-
 import javax.persistence.EntityManager;
-import javax.persistence.EntityNotFoundException;
 import javax.persistence.PersistenceContext;
+
+import java.util.ArrayList;
 import java.util.List;
+
 
 @Repository
 public class UserDAOImpl implements UserDAO {
 
+    private List<User> users;
+
     @PersistenceContext
     private EntityManager em;
+
+    @Autowired
+    public UserDAOImpl(List<User> users) {
+        users = new ArrayList<User>();
+        users.add(new User("FirstName", "FirstSurname", (byte) 11));
+        users.add(new User("FirstName", "FirstSurname", (byte) 11));
+    }
 
     @Override
     public List<User> allUsers() {
@@ -26,7 +38,7 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public User getOne(long id) {
-        return em.find(User.class, id);
+        return em.createQuery("select u from User u where u.id = :id", User.class).setParameter("id", id).getSingleResult();
     }
 
     @Override
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDAO;
 import web.model.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,6 +23,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void saveUser(User user) {
         userDAO.saveUser(user);
     }
@@ -31,11 +34,13 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void updateUser(Long id, User user) {
         userDAO.updateUser(id, user);
     }
 
     @Override
+    @Transactional
     public void deleteUser(Long id) {
         userDAO.deleteUser(id);
     }
